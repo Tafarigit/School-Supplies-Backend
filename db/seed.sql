d344e031df4f597a6a69330c56680c2594ab8b5f
ariunaamy <ariun.myagmar@gmail.com>
2023-05-12 19:05:07 -0400

    data added to db

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,8 @@
 \c supplies_dev; 
 INSERT INTO supplies (name, brand, image_URL, price, quantity, description, in_stock)
-VALUES ('pencil','Faber-Castel', 'https://cdn.shopify.com/s/files/1/0049/6261/5365/products/vhskidfi2szjlojl9bhd_2048x.jpg?v=1668279862', 1.99, 1, 'pencil with rubber eraser', true);
+VALUES ('pencil','Faber-Castel', 'https://cdn.shopify.com/s/files/1/0049/6261/5365/products/apwnljgg2glekxgjjaw4_2048x.jpg?v=1668278678', 1.99, 1, 'pencil with rubber eraser', true),
+('pencil','Faber-Castel', 'https://cdn.shopify.com/s/files/1/0049/6261/5365/products/apwnljgg2glekxgjjaw4_2048x.jpg?v=1668278678', 1.99, 1, 'pencil with rubber eraser', false),
+('pencil','Faber-Castel', 'https://cdn.shopify.com/s/files/1/0049/6261/5365/products/apwnljgg2glekxgjjaw4_2048x.jpg?v=1668278678', 1.99, 1, 'pencil with rubber eraser', true);
 
 -- \c supplies_dev; 
 -- INSERT INTO supplies (name, brand, image_URL, price, quantity, description, in_stock)
